90400f2904d9c844c0853961b8285df9b0c7a143
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-02-29 06:45:52 +0100

    completed mobile view -light-mode-only

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/headerComponent/Header"
+import Search from "./components/searchComponent/Search"
+import ProfileCard from "./components/profilecardComponent/ProfileCard"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Search />
+      <ProfileCard />
+    </>
+  );
+}
+
+export default App
