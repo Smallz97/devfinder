a96364c98f3745cc0ca324a0216d43b235d7d505
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-03-11 18:22:30 +0100

    refactored codebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useSelector } from "react-redux"
 import Header from "./components/headerComponent/Header"
 import Search from "./components/searchComponent/Search"
+import Loader from "./components/loadingComponent/Loader"
 import ProfileCard from "./components/profilecardComponent/ProfileCard"
 import './App.css'
 
 function App() {
+  const { loading } = useSelector((state) => state.user);
   return (
     <>
-      <Header/>
+      <Header />
       <Search />
-      <ProfileCard />
+      {loading ? <Loader/> : <ProfileCard />}
     </>
   );
 }
