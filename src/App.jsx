6e80462168ca75b0599cd94097c070a346a6a7d0
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-03-23 00:29:20 +0100

    implemented theme toggling feature with theme switch button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux"
 import Header from "./components/headerComponent/Header"
 import Search from "./components/searchComponent/Search"
 import ProfileCard from "./components/profilecardComponent/ProfileCard"
 import './App.css'
 
 function App() {
-  const { loading } = useSelector((state) => state.user);
   return (
     <>
       <Header />
--- src/app/userSlice.js
+++ src/app/userSlice.js
@@ -1,75 +1,91 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const setUserSlice = createSlice ({
-    name: 'user',
-    initialState: {
-        user: {},
-        theme: 'light',
-        loaded: false,
-        loading: false,
-        notFound: false,
-    },
-    reducers: {
-        userLoading: (state) => {
-            state.loading = true;
-        },
-        userNotFound(state) {
-            state.loading = false;
-            state.notFound = true;
-        },
-        userReceived(state, action) {  
-            state.loading = false;
-            if (state.notFound) {
-                state.notFound = false
-            };
-            state.user = action.payload;
-            state.loaded = true;
-        },
-        toggleTheme(state) {
-            state.theme = state.theme === 'light' ? 'dark' : 'light';
-        },
-    }
-})
+function isDarkThemePreferred() {
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+}
 
-export const { userLoading, userNotFound, userReceived, toggleTheme } = setUserSlice.actions
+const initialTheme = isDarkThemePreferred() ? "dark" : "light";
+
+export const setUserSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: {},
+    loaded: false,
+    loading: false,
+    notFound: false,
+    theme: initialTheme,
+  },
+  reducers: {
+    userLoading: (state) => {
+      state.loading = true;
+    },
+    userNotFound(state) {
+      state.loading = false;
+      state.notFound = true;
+    },
+    userReceived(state, action) {
+      state.loading = false;
+      if (state.notFound) {
+        state.notFound = false;
+      }
+      state.user = action.payload;
+      state.loaded = true;
+    },
+    toggleTheme(state) {
+      const body = document.querySelector("body");
+      state.theme === "light"
+        ? (body.classList.add("dark-theme"),
+          body.classList.remove("light-theme"))
+        : (body.classList.add("light-theme"),
+          body.classList.remove("dark-theme"));
+      state.theme = state.theme === "light" ? "dark" : "light";
+      console.log(body);
+    },
+  },
+});
+
+export const { userLoading, userNotFound, userReceived, toggleTheme } =
+  setUserSlice.actions;
 
 // Defining a thunk that dispatches the action creators with async functions
 export const fetchUsers = (user) => async (dispatch) => {
-    const BaseGithubApi = 'https://api.github.com/users';
-    dispatch(userLoading())
-    
-    try {
-        const fetchUserData = await fetch(`${BaseGithubApi}/${user}`)
-        const isResponseOk = fetchUserData.ok;
+  const BaseGithubApi = "https://api.github.com/users";
+  dispatch(userLoading());
 
-        if(isResponseOk) {
-            const userData = await fetchUserData.json();
+  try {
+    const fetchUserData = await fetch(`${BaseGithubApi}/${user}`);
+    const isResponseOk = fetchUserData.ok;
 
-            const userDetails = {
-            bio: userData.bio,
-            blog: userData.blog,
-            login: userData.login,
-            fullname: userData.name,
-            company: userData.company,
-            avatar: userData.avatar_url,
-            location: userData.location,
-            followers: userData.followers,
-            following: userData.following,
-            dateJoined: userData.created_at,
-            totalRepos: userData.public_repos,
-            twitter: userData.twitter_username,
-            gitHubProfileUrl: userData.html_url,
-            };
+    if (isResponseOk) {
+      const userData = await fetchUserData.json();
 
-            dispatch(userReceived(userDetails))
-        } else {
-            fetchUserData.status === 404
-            dispatch(userNotFound());
-        }
+      const userDetails = {
+        bio: userData.bio,
+        blog: userData.blog,
+        login: userData.login,
+        fullname: userData.name,
+        company: userData.company,
+        avatar: userData.avatar_url,
+        location: userData.location,
+        followers: userData.followers,
+        following: userData.following,
+        dateJoined: userData.created_at,
+        totalRepos: userData.public_repos,
+        twitter: userData.twitter_username,
+        gitHubProfileUrl: userData.html_url,
+      };
 
-    } catch (error) {
-        console.error('Fetch error:', error);
+      dispatch(userReceived(userDetails));
+    } else {
+      fetchUserData.status === 404;
+      dispatch(userNotFound());
     }
-}
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
 
-export default setUserSlice.reducer
+export default setUserSlice.reducer;
